c514f90cdd0a241d19cfef485faacf5337b7419d
Rogachev_vv <vlad2320082009@gmail.com>
2022-05-11 10:10:38 +0400

    Время добавил

--- app/src/main/java/com/example/sorttask/MainActivity.kt
+++ app/src/main/java/com/example/sorttask/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.sorttask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.TimingLogger
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import java.util.*
+import java.util.Collections.swap
 
 class MainActivity : AppCompatActivity() {
     private val random = Random()
-    private fun rand() : Int {
+    private fun rand(): Int {
         return random.nextInt(1000)
     }
+
     var arrayList: MutableList<Int> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +23,15 @@ class MainActivity : AppCompatActivity() {
         val tvText: TextView = findViewById(R.id.tvArray)
         val btnCount: Button = findViewById(R.id.btnCount)
         btnCount.setOnClickListener {
-            val count:EditText = findViewById(R.id.etCount)
-            for(i in 1..count.text.toString().toInt()){
+            tvText.text = ""
+            arrayList.clear()
+            val count: EditText = findViewById(R.id.etCount)
+            for (i in 1..count.text.toString().toInt()) {
                 arrayList.add(rand())
             }
             //arrayList.sort()
             val sBuilder = StringBuilder()
-            for((counter, i) in (1..arrayList.size).withIndex()){
+            for ((counter, i) in (1..arrayList.size).withIndex()) {
                 if (i == arrayList.size) sBuilder.append("${arrayList[counter]}")
                 else sBuilder.append("${arrayList[counter]},")
             }
@@ -33,81 +39,110 @@ class MainActivity : AppCompatActivity() {
         }
         val btnOne: Button = findViewById(R.id.btnOne)
         btnOne.setOnClickListener {
-            var counter = 0
+            val startTime = System.nanoTime()
             arrayList = foolSortAlgorithm(arrayList)
+            val totalTime = System.nanoTime() - startTime
+            Log.d("MyLog", totalTime.toString())
             val aBuilder = StringBuilder()
-            for(i in 1..arrayList.size){
+            for ((counter, i) in (1..arrayList.size).withIndex()) {
                 if (i == arrayList.size) aBuilder.append("${arrayList[counter]}")
                 else aBuilder.append("${arrayList[counter]},")
-                ++counter
             }
             tvText.text = aBuilder.toString()
         }
         val btnTwo: Button = findViewById(R.id.btnTwo)
         btnTwo.setOnClickListener {
-            var counter = 0
+            val startTime = System.nanoTime()
             arrayList = bubbleSortFromFool(arrayList)
+            val totalTime = System.nanoTime() - startTime
+            Log.d("MyLog", totalTime.toString())
             val aBuilder = StringBuilder()
-            for(i in 1..arrayList.size){
+            for ((counter, i) in (1..arrayList.size).withIndex()) {
                 if (i == arrayList.size) aBuilder.append("${arrayList[counter]}")
                 else aBuilder.append("${arrayList[counter]},")
-                ++counter
             }
             tvText.text = aBuilder.toString()
         }
         val btnThree: Button = findViewById(R.id.btnThree)
         btnThree.setOnClickListener {
-            var counter = 0
+            val startTime = System.nanoTime()
             arrayList = cocktailSortAlgorithm(arrayList)
+            val totalTime = System.nanoTime() - startTime
+            Log.d("MyLog", totalTime.toString())
             val aBuilder = StringBuilder()
-            for(i in 1..arrayList.size){
+            for ((counter, i) in (1..arrayList.size).withIndex()) {
                 if (i == arrayList.size) aBuilder.append("${arrayList[counter]}")
                 else aBuilder.append("${arrayList[counter]},")
-                ++counter
+            }
+            tvText.text = aBuilder.toString()
+        }
+        val btnFour: Button = findViewById(R.id.btnFour)
+        btnFour.setOnClickListener {
+            val startTime = System.nanoTime()
+            arrayList = combSortAlgorithm(arrayList)
+            val totalTime = System.nanoTime() - startTime
+            Log.d("MyLog", totalTime.toString())
+            val aBuilder = StringBuilder()
+            for ((counter, i) in (1..arrayList.size).withIndex()) {
+                if (i == arrayList.size) aBuilder.append("${arrayList[counter]}")
+                else aBuilder.append("${arrayList[counter]},")
+            }
+            tvText.text = aBuilder.toString()
+        }
+        val btnFive: Button = findViewById(R.id.btnFive)
+        btnFive.setOnClickListener {
+            val startTime = System.nanoTime()
+            arrayList = shellSort(arrayList)
+            val totalTime = System.nanoTime() - startTime
+            Log.d("MyLog", totalTime.toString())
+            val aBuilder = StringBuilder()
+            for ((counter, i) in (1..arrayList.size).withIndex()) {
+                if (i == arrayList.size) aBuilder.append("${arrayList[counter]}")
+                else aBuilder.append("${arrayList[counter]},")
             }
             tvText.text = aBuilder.toString()
         }
     }
 
-    //Пузырьковая Сортировка
-    private fun bubbleSortFromFool(items: MutableList<Int>):MutableList<Int>{
-        do{
-            var swapped = false
-            for(index in 1 until items.size){
-                val previousItem = items[index-1]
-                val currentItem = items[index]
-                if(previousItem.compare(currentItem)){
-                    items.swapItems(index-1, index)
-                    swapped = true
-                }
-            }
-        }while (swapped)
-        return items
-    }
-
 
     //Глупая Сортировка
-    private fun foolSortAlgorithm(items: MutableList<Int>):MutableList<Int>{
+    private fun foolSortAlgorithm(items: MutableList<Int>): MutableList<Int> {
         var swapped = true
-        while (swapped){
+        while (swapped) {
             swapped = swapIfNeedAndReturn(items)
         }
         return items
     }
 
-    private fun swapIfNeedAndReturn(items: MutableList<Int>):Boolean{
-        for(index in 1 until items.size){
-            val previousItem = items[index-1]
+    //Пузырьковая Сортировка
+    private fun bubbleSortFromFool(items: MutableList<Int>): MutableList<Int> {
+        do {
+            var swapped = false
+            for (index in 1 until items.size) {
+                val previousItem = items[index - 1]
+                val currentItem = items[index]
+                if (previousItem.compare(currentItem)) {
+                    items.swapItems(index - 1, index)
+                    swapped = true
+                }
+            }
+        } while (swapped)
+        return items
+    }
+
+    private fun swapIfNeedAndReturn(items: MutableList<Int>): Boolean {
+        for (index in 1 until items.size) {
+            val previousItem = items[index - 1]
             val currentItem = items[index]
-            if(previousItem.compare(currentItem)){
-                items.swapItems(index-1, index)
+            if (previousItem.compare(currentItem)) {
+                items.swapItems(index - 1, index)
                 return true
             }
         }
         return false
     }
 
-    private fun MutableList<Int>.swapItems(leftIndex: Int, rightIndex: Int){
+    private fun MutableList<Int>.swapItems(leftIndex: Int, rightIndex: Int) {
         if (leftIndex != rightIndex) {
             val temporary = this[leftIndex]
             this[leftIndex] = this[rightIndex]
@@ -115,11 +150,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun Int.compare(anotherItem:Int):Boolean{
-        return this-anotherItem>0
+    private fun Int.compare(anotherItem: Int): Boolean {
+        return this - anotherItem > 0
     }
 
-    private fun cocktailSortAlgorithm(items: MutableList<Int>): MutableList<Int>{
+    //Шейкерная сортировка
+    private fun cocktailSortAlgorithm(items: MutableList<Int>): MutableList<Int> {
         var rightBorder = items.size
         var leftBorder = 1
 
@@ -130,7 +166,7 @@ class MainActivity : AppCompatActivity() {
                 val currentItem = items[index]
                 if (previousItem.compare(currentItem)) {
                     items.swapItems(index - 1, index)
-                    rightBorder = index-1
+                    rightBorder = index - 1
                     swapped = true
                 }
             }
@@ -154,6 +190,56 @@ class MainActivity : AppCompatActivity() {
         do {
             val swapped: Boolean = swapIfNeedGoUp(items) && swapIfNeedGoDown(items)
         } while (swapped)
-    return items
+        return items
+    }
+
+    //Расческой
+    private fun combSortAlgorithm(items: MutableList<Int>): MutableList<Int> {
+        var gap = items.size
+        var swapped = true
+        var index: Int
+
+        while (gap > 1 || swapped) {
+            if (gap > 1) {
+                gap = (gap / 1.3).toInt()
+            }
+            swapped = false
+            index = 0
+            while (index + gap < items.size) {
+                if (items[index] > items[index + gap]) {
+                    items.swapItems(index, index + gap)
+                }
+                index++
+            }
+        }
+        return items
+    }
+
+    //Вставкой
+    private fun shellSort(array: MutableList<Int>): MutableList<Int> {
+        var interval = 1
+        var insertedValue: Int
+        var insertedIndex: Int
+        //получаем первоначальный интервал, с которым буем ходить по массиву
+        //согласно круту, интервал расчитывается по формуле h = 3*h + 1
+        while (interval < array.size / 3) {
+            interval = 3 * interval + 1
+        } //используем метод сортировки вставками на большом интервале. //Как только интервал становится равен 1 - сортировка становится обычной сортировкой вставками
+        while (interval > 0) {
+            for (i in interval until array.size) {
+                insertedValue = array[i]
+                insertedIndex = i
+
+                while (insertedIndex >= interval && insertedValue < array[insertedIndex - interval]) {
+                    array[insertedIndex] = array[insertedIndex - interval]
+                    insertedIndex -= interval
+                }
+
+                array[insertedIndex] = insertedValue
+            }
+
+            interval = (interval - 1) / 3
+        }
+        return array
     }
 }
